2adea3d774332d1433cf52734db60d103bd97516
Collinsk70 <collinskarweru@gmail.com>
2023-12-06 00:58:08 +0000

    commit

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,5 @@
+e containing the function read_file"""
 
-#!/usr/bin/python3
-"""Module containing the function read_file"""
 
 def read_file(filename=""):
     """Reads a file and prints to stdout.
